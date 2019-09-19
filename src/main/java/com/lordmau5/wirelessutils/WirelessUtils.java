050734899641188c1ab56717da02f8c5e598fecf
SirStendec <sir@stendec.me>
2019-09-18 21:01:46 -0400

    Bump version to 1.9

--- src/main/java/com/lordmau5/wirelessutils/WirelessUtils.java
+++ src/main/java/com/lordmau5/wirelessutils/WirelessUtils.java
@@ -28,7 +28,7 @@ public class WirelessUtils {
     public static final String NAME = "Wireless Utilities";
     public static final int DATA_VERSION = 1;
 
-    public static final String VERSION = "1.8";
+    public static final String VERSION = "1.9";
     public static final String UPDATE_URL = "https://raw.github.com/sirstendec/wirelessutilities/master/update.json";
 
     public static final String DEPENDENCIES = CoFHCore.VERSION_GROUP;
